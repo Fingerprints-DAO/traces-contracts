d20de362af9584afed2b1aa2eef76aec034fb150
arodundef <arod.mail@protonmail.com>
2022-09-27 18:18:55 -0300

    chore(scripts): add fundamental scripts to run
    run node; deploy local; test watch; run docs and compile

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,74 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from 'dotenv'
+import { HardhatUserConfig, task } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox'
+import 'hardhat-watcher'
+import 'hardhat-contract-sizer'
+import 'hardhat-docgen'
+import 'dotenv/config'
+
+// import 'hardhat-docgen'
+
+dotenv.config()
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners()
+
+  for (const account of accounts) {
+    console.log(account.address)
+  }
+})
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+const accounts = {
+  mnemonic:
+    process.env.MNEMONIC || 'abc abc abc abc abc abc abc abc abc abc abc abc',
+}
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
-};
+  solidity: '0.8.17',
+  networks: {
+    hardhat: {
+      chainId: 1337,
+      accounts: {
+        accountsBalance: '10000000000000000000000',
+      },
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts,
+      chainId: 4,
+      // @ts-ignore
+      live: true,
+      saveDeployments: true,
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: 'USD',
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  watcher: {
+    test: {
+      tasks: [{ command: 'test', params: { testFiles: ['{path}'] } }],
+      files: ['./test/**/*'],
+      verbose: true,
+    },
+  },
+  contractSizer: {
+    disambiguatePaths: false,
+    runOnCompile: true,
+    strict: process.env.CONTRACT_SIZER_STRICT_DISABLE ? false : true,
+  },
+  docgen: {
+    path: './docs',
+    clear: true,
+    runOnCompile: true,
+  },
+}
 
-export default config;
+export default config
