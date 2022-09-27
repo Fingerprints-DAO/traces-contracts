affdc24985974402610e52c09c0d1e70921e5369
arodundef <arod.mail@protonmail.com>
2022-09-27 19:59:43 -0300

    test(vaultAddress): add successful test

--- test/Traces.ts
+++ test/Traces.ts
@@ -10,13 +10,13 @@ describe('Traces', function () {
   // and reset Hardhat Network to that snapshot in every test.
   async function deployFixture() {
     // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await ethers.getSigners()
+    const [deployer, owner] = await ethers.getSigners()
     const FPVaultAddress = faker.finance.ethereumAddress()
 
     const Traces = await ethers.getContractFactory('Traces')
-    const trace = await Traces.deploy(otherAccount.address, FPVaultAddress)
+    const trace = await Traces.deploy(owner.address, FPVaultAddress)
 
-    return { trace, owner, otherAccount, FPVaultAddress }
+    return { trace, deployer, owner, FPVaultAddress }
   }
 
   describe('Traces Deployment', function () {
@@ -26,9 +26,9 @@ describe('Traces', function () {
       expect(await trace.totalSupply()).to.eq(0)
     })
     it('transfers contract ownership to owner address sent', async function () {
-      const { trace, otherAccount } = await loadFixture(deployFixture)
+      const { trace, owner } = await loadFixture(deployFixture)
 
-      expect(await trace.owner()).to.eq(otherAccount.address)
+      expect(await trace.owner()).to.eq(owner.address)
     })
     it('saves FP address when deploying', async function () {
       const { trace, FPVaultAddress } = await loadFixture(deployFixture)
@@ -45,6 +45,16 @@ describe('Traces', function () {
 
       await expect(trace.setVaultAddress(faker.finance.ethereumAddress())).to
         .reverted
+    })
+    it('returns properly the new vaultAddress after executed setVaultAddress', async function () {
+      const { trace, owner } = await loadFixture(deployFixture)
+      const newVaultAddress = faker.finance.ethereumAddress()
+
+      await trace.connect(owner).setVaultAddress(newVaultAddress)
+
+      expect(await trace.vaultAddress()).to.match(
+        new RegExp(newVaultAddress, 'i')
+      )
     })
   })
 })
