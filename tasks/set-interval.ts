4828be90b86750c190de97f127ed57fc373de85f
arodundef <arod.mail@protonmail.com>
2023-01-03 17:48:08 -0300

    chore(setInterval): get parameter from terminal

--- tasks/set-interval.ts
+++ tasks/set-interval.ts
@@ -1,8 +1,13 @@
-import { task } from 'hardhat/config'
+import { task, types } from 'hardhat/config'
 
-task('set-interval', 'Change mining interval').setAction(
-  async (_, { ethers, network }) => {
+task('set-interval', 'Change mining interval')
+  .addOptionalParam(
+    'interval',
+    'The interval in milliseconds',
+    '100000',
+    types.string
+  )
+  .setAction(async ({ interval }, { ethers, network }) => {
     await network.provider.send('evm_setAutomine', [false])
-    await ethers.provider.send('evm_setIntervalMining', [100000])
-  }
-)
+    await ethers.provider.send('evm_setIntervalMining', [interval])
+  })
